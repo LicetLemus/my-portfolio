39d74479c998db2f2ea90d99893d41c4353cd954
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-10-21 11:17:47 -0500

    update: button prev and next for Slider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js'
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 // Import your SCSS file here
 import './styles/styles.scss';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
